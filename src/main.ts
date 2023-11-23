960ae07a14c730a0bf2c40ade5257546be06989a
keiouksliu <keiouksliu@futunn.com>
2023-11-23 09:46:46 +0800

    feat: clear all

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-import './assets/main.css'
+import './assets/base.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 const app = createApp(App);
 const app2 = createApp(App);
-
-app.component('HelloWorld', HelloWorld);
-app2.component('HelloWorld', HelloWorld);
 
 app.mount('#app');
 app2.mount('#app2');
